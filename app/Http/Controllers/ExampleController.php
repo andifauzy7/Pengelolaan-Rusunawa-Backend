5106198f0fe8993651f0f87c40077b9c721ad24a
Andi Fauzy Dewantara <andi.fauzy.tif18@polban.ac.id>
2021-07-21 03:10:55 +0700

    first commit

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    // Pengguna 
+    public function validasiPengguna(Request $request){
+
+    }
+
+    public function loginPengguna(Request $request){
+
+    }
+
+    public function registerPengguna(Request $request){
+
+    }
+
+    public function getPengguna(Request $request){
+
+    }
+
+    public function editPengguna(Request $request){
+
+    }
+
+    // Rusunawa.
+    public function getSemuaRusunawa(Request $request){
+
+    }
+
+    public function getRusunawa(Request $request, String $id){
+
+    }
+
+    public function editRusunawa(Request $request, String $id){
+
+    }
+
+    public function deleteRusunawa(Request $request, String $id){
+
+    }
+
+    
+}
